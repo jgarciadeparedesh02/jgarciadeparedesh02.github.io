56b28bd0e44d8d6bbb621468ccce5414cf07bb31
jgarciadeparedesh02 <jgarciadeparedesh02@educarex.es>
2024-10-02 10:11:04 +0200

    preparing new version

--- prepare-new-version-copy-to-temp.sh
+++ prepare-new-version-copy-to-temp.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Update Submodules Apuntes
+echo "--------------------------------------------------------------------------------"
+echo "--- Updating submodules"
+echo "--------------------------------------------------------------------------------"
 git submodule update --remote
 # Regenerate the site html
+echo "--------------------------------------------------------------------------------"
+echo "--- Generating new site html"
+echo "--------------------------------------------------------------------------------"
 mkdocs-multirepo --build
 # Delete and copy the file index.html to site
 rm site/index.html
@@ -10,6 +16,9 @@ cp index.html.sec site/index.html
 # Copy site to temp
 cp -r site ../tmp
 # Git add and commit
+echo "--------------------------------------------------------------------------------"
+echo "--- Committing new version"
+echo "--------------------------------------------------------------------------------"
 git add .
 git commit -m "preparing new version"
 git push
