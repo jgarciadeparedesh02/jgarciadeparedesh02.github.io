da720d53922a82cb12cf7af9441fe1b8f4504d5c
jgarciadeparedesh02 <jgarciadeparedesh02@educarex.es>
2024-10-04 15:15:27 +0200

    preparing new version

--- prepare-new-version-copy-to-temp.sh
+++ prepare-new-version-copy-to-temp.sh
@@ -10,6 +10,21 @@ echo "--------------------------------------------------------------------------
 echo "--- Generating new site html"
 echo "--------------------------------------------------------------------------------"
 mkdocs-multirepo --build
+# Generating sites for every submodule cause multidocs dont support ganalitys
+cd src/dwec-apuntes
+mkdocs build
+cd ../../src/di-apuntes
+mkdocs build
+cd ../../src/bda-apuntes
+mkdocs build
+cd ../../
+rm -rf site/dwec-apuntes
+rm -rf site/di-apuntes
+rm -rf site/bda-apuntes
+cp -r src/dwec-apuntes/site site/dwec-apuntes
+cp -r src/di-apuntes/site site/di-apuntes
+cp -r src/bda-apuntes/site site/bda-apuntes
+
 # Delete and copy the file index.html to site
 rm site/index.html
 cp index.html.sec site/index.html
